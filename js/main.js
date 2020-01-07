e98553734fcfeb62a6688ace53cb3583c8f25343
Abay <abayibrayev7@gmail.com>
2020-01-07 20:59:36 +0000

    added responsive hero section

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
   const dark = document.getElementById('dark-toggle'),
         lang = document.getElementById('lang-toggle'),
-        ball = document.querySelectorAll('.ball');
+        ball = document.querySelectorAll('.ball'),
+        arrow = document.querySelector('.arrow');
 
   dark.addEventListener('change', () => {
     document.body.classList.toggle('dark');
     ball.forEach((item) => { item.classList.toggle('ball_dark'); });
+    arrow.classList.toggle('arrow_dark');
   });
 
 /* ----------------------------- language change ---------------------------- */
