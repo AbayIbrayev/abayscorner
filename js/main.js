3f8a584995a88e3f2adfd4fd946fbaab43b2fa0d
Abay <abayibrayev7@gmail.com>
2020-01-08 22:21:34 +0000

    added hamburger menu and navigation animations

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 window.addEventListener('DOMContentLoaded', function() {
-/* ----------------------------- darkmode change ---------------------------- */
+/* ----------------------------- hamburger menu ----------------------------- */
+
+  const menuToggle = document.querySelector('.menu-toggle'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        nav = document.querySelector('.nav'),
+        section = document.querySelectorAll('.section');
+
+  menuToggle.addEventListener('click', () => {
+    menuItem.forEach((item) => { item.classList.toggle('menu__item_open'); });
+    section.forEach((item) => { item.classList.toggle('section_open'); });
+    menuToggle.classList.toggle('menu-toggle_open');
+    nav.classList.toggle('nav_open');
+  });
+
+  /* ----------------------------- darkmode change ---------------------------- */
 
   const dark = document.getElementById('dark-toggle'),
-        lang = document.getElementById('lang-toggle'),
-        ball = document.querySelectorAll('.ball'),
-        arrow = document.querySelector('.arrow');
+    lang = document.getElementById('lang-toggle'),
+    ball = document.querySelectorAll('.ball'),
+    arrow = document.querySelector('.arrow'),
+    menuToggleDark = document.querySelectorAll('.menu-toggle'),
+    navDark = document.querySelectorAll('.nav'),
+    menuItemDark = document.querySelectorAll('.menu__item'),
+    menuLinkDark = document.querySelectorAll('.menu__link');
 
   dark.addEventListener('change', () => {
     document.body.classList.toggle('dark');
     ball.forEach((item) => { item.classList.toggle('ball_dark'); });
     arrow.classList.toggle('arrow_dark');
+    menuToggleDark.forEach((item) => { item.classList.toggle('menu-toggle_dark'); });
+    navDark.forEach((item) => { item.classList.toggle('nav_dark'); });
+    menuItemDark.forEach((item) => { item.classList.toggle('menu__item_dark'); });
+    menuLinkDark.forEach((item) => { item.classList.toggle('menu__link_dark'); });
   });
 
-/* ----------------------------- language change ---------------------------- */
+  /* ----------------------------- language change ---------------------------- */
 
   lang.addEventListener('change', () => {
-    
+
   });
+
 });
