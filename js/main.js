4de2453e14cb2dfd56b2ea915569f5fdfaed4d26
Abay <abayibrayev7@gmail.com>
2020-01-09 07:20:28 +0000

    hero changes

--- js/main.js
+++ js/main.js
@@ -16,13 +16,12 @@ window.addEventListener('DOMContentLoaded', function() {
   /* ----------------------------- darkmode change ---------------------------- */
 
   const dark = document.getElementById('dark-toggle'),
-    lang = document.getElementById('lang-toggle'),
-    ball = document.querySelectorAll('.ball'),
-    arrow = document.querySelector('.arrow'),
-    menuToggleDark = document.querySelectorAll('.menu-toggle'),
-    navDark = document.querySelectorAll('.nav'),
-    menuItemDark = document.querySelectorAll('.menu__item'),
-    menuLinkDark = document.querySelectorAll('.menu__link');
+        lang = document.getElementById('lang-toggle'),
+        ball = document.querySelectorAll('.ball'),
+        arrow = document.querySelector('.arrow'),
+        menuToggleDark = document.querySelectorAll('.menu-toggle'),
+        navDark = document.querySelectorAll('.nav'),
+        menuLinkDark = document.querySelectorAll('.menu__link');
 
   dark.addEventListener('change', () => {
     document.body.classList.toggle('dark');
@@ -30,7 +29,7 @@ window.addEventListener('DOMContentLoaded', function() {
     arrow.classList.toggle('arrow_dark');
     menuToggleDark.forEach((item) => { item.classList.toggle('menu-toggle_dark'); });
     navDark.forEach((item) => { item.classList.toggle('nav_dark'); });
-    menuItemDark.forEach((item) => { item.classList.toggle('menu__item_dark'); });
+    menuItem.forEach((item) => { item.classList.toggle('menu__item_dark'); });
     menuLinkDark.forEach((item) => { item.classList.toggle('menu__link_dark'); });
   });
 
