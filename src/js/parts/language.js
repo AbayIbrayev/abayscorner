9183571d6912734109b0b49de1e9e4ea8b36b4c8
Abay <abayibrayev7@gmail.com>
2020-02-11 14:51:44 +0000

    minor changes

--- src/js/parts/language.js
+++ src/js/parts/language.js
@@ -2,18 +2,18 @@ function language() {
   /* ----------------------------- language change ---------------------------- */
 
   const heroTitle = document.querySelector('.hero__title'),
-        lang = document.getElementById('lang-toggle'),
-        heroDescription = document.querySelector('.hero__description'),
-        projectsTitle = document.querySelector('#projects div h2'),
-        recentProjects = document.querySelectorAll('.projects-header-tab')[0],
-        personalProjects = document.querySelectorAll('.projects-header-tab')[1],
-        funProjects = document.querySelectorAll('.projects-header-tab')[2],
-        skillsTitle = document.querySelector('#skills div h2'),
-        contactTitle = document.querySelector('#contact div h2'),
-        homeLink = document.querySelectorAll('.menu__item')[0],
-        projectsLink = document.querySelectorAll('.menu__item')[1],
-        skillsLink = document.querySelectorAll('.menu__item')[2],
-        contactLink = document.querySelectorAll('.menu__item')[3];
+    lang = document.getElementById('lang-toggle'),
+    heroDescription = document.querySelector('.hero__description'),
+    projectsTitle = document.querySelector('#projects div h2'),
+    recentProjects = document.querySelectorAll('.projects-header-tab')[0],
+    personalProjects = document.querySelectorAll('.projects-header-tab')[1],
+    funProjects = document.querySelectorAll('.projects-header-tab')[2],
+    skillsTitle = document.querySelector('#skills div h2'),
+    contactTitle = document.querySelector('#contact div h2'),
+    homeLink = document.querySelectorAll('.menu__item')[0],
+    projectsLink = document.querySelectorAll('.menu__item')[1],
+    skillsLink = document.querySelectorAll('.menu__item')[2],
+    contactLink = document.querySelectorAll('.menu__item')[3];
 
   let langKey = localStorage.getItem('langKey');
 
